f271ca9bef83cf700e223aa1b409f4e3fc4022de
sabzdotpy <incrediblesabari02@gmail.com>
2022-06-02 09:39:42 +0530

    added search functionality in questions page

--- src/pages/linkedin-practices.tsx
+++ src/pages/linkedin-practices.tsx
@@ -6,7 +6,35 @@ export const LinkedInPracticesPage: React.FC = () => {
   const { encodedUrl } = useParams<{ encodedUrl: string }>();
   const url = atob(encodedUrl || '');
 
+  const handleFilter = (event: React.ChangeEvent<HTMLInputElement>) => {
+		let questions = document.querySelectorAll(
+			'[aria-label="question-container"]'
+		);
+
+		questions.forEach((item: Element, index: number) => {
+			(questions[index].parentNode?.parentNode as HTMLElement).style.display =
+				item.textContent
+					?.toLowerCase()
+					.includes(event.target.value.toLowerCase())
+					? "block"
+					: "none";
+		});
+	};
+
   return (
-    <GithubPractices githubLink={url} />
+    <React.Fragment>
+      <div className="w-full">
+        <input
+          className="rounded-full w-full p-3 mt-10 border-2 bg-primary-dark-800"
+          type="search"
+          name="filter-linkedin"
+          id="filter-linkedin"
+          autoComplete="off"
+          placeholder="Filter"
+          onChange={handleFilter}
+        />
+      </div>
+			<GithubPractices githubLink={url} />
+		</React.Fragment>
   );
 };
